8f3b4324e1485b38ff9f37770fab489138a655d1
Kenny Krosky <kenny.krosky@gmail.com>
2018-05-10 22:37:40 -0700

    updates to speed

--- footer.php
+++ footer.php
@@ -1,12 +1,51 @@
 <?php $root = get_template_directory_uri(); ?>
 
-
-<section>
-
-</section>
+<?php get_template_part('partials/pre-footer'); ?>
 </main>
 
-<footer></footer>
+
+
+<footer>
+  <div class="save-today">
+    <div class="inner is-centered">
+      <a href="">SAVE 20% TODAY FOR ALL BEE SERVICES  </a>
+    </div>
+  </div>
+
+  <div class="footer">
+    <div class="inner">
+      <div class="three-col--footer">
+        <div class="foot-el">
+          <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Photo Album</a></li>
+            <li><a href="">Faq</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Contact</a></li>
+          </ul>
+        </div>
+        <div class="foot-el">
+          <div class="outer-logo-container">
+            <figure class="footer-logo">
+              <img class="lazy-image" src="" data-lazy="<?php echo "$root/dist/images/logo.png";?>" alt="jr bees logo">
+            </figure>
+            &copy; <?php echo date('Y'); ?>
+          </div>
+        </div>
+        <div class="foot-el">
+          <ul>
+            <li><a href="">Facebook</a></li>
+            <li><a href="">Instagram</a></li>
+            <li><a href="">Yelp</a></li>
+            <li><a href="https://www.linkedin.com/in/jesse-adcock-9735a5106/?trk=nav_responsive_tab_profile_pic">Linkedin</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+</footer>
 <script src="<?php echo "$root/dist/bundle.js"; ?> "></script>
 <?php wp_footer(); ?>
 </body>
---
+++ partials/pre-footer.php
@@ -0,0 +1,49 @@
+<?php $root = get_template_directory_uri(); ?>
+
+
+<section class="is-parent-el--padding">
+  <div class="inner">
+    <div class="two-cta--section">
+
+      <div class="is-cta-el">
+        <div class="is-cta--liner">
+          <h1>Here's a Cta</h1>
+          <h4>Sub Title</h4>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita, repellat? Distinctio explicabo hic sint iusto perferendis dignissimos quas id earum sit et? Eos cum quisquam reprehenderit incidunt necessitatibus ab molestiae?</p>
+          <a href="" class="cta">
+            SCHEDULE TODAY!
+          </a>
+        </div>
+      </div>
+
+      <div class="is-cta-el is-centered">
+        <div class="is-cta--liner">
+          <h1>Newsletter</h1>
+          <h4>Catch Up With Great Content</h4>
+          <div class="form-cta">
+            <form action="">
+              <div class="input">
+                <input type="email" placeholder="email@gmail.com">
+                <button class="arrow-btn" type="submit">
+                <svg width="29px" height="24px" viewBox="0 0 29 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                  <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round">
+                      <g id="Desktop-HD" transform="translate(-1277.000000, -3896.000000)" stroke="#FFFFFF" stroke-width="4">
+                          <g id="arrow-right" transform="translate(1279.000000, 3898.000000)">
+                              <path d="M0,10.5 L24,10.5" id="Shape"></path>
+                              <polyline id="Shape" points="15 0 25 10 15 20"></polyline>
+                          </g>
+                      </g>
+                  </g>
+              </svg>
+                </button>
+              </div>
+            </form>
+          </div>
+          
+        </div>
+        <div class="is-hero--bg is-bg" data-bg-src="<?php echo "$root/dist/images/cta.jpg"; ?>"></div>
+      </div>
+    
+    </div>
+  </div>
+</section>
