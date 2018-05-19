60c334799fa3b23d47ee7fc622148bdec4fa4273
Kenny Krosky <kenny.krosky@gmail.com>
2018-05-18 22:05:11 -0700

    updates on the blog page

---
+++ single.php
@@ -0,0 +1,39 @@
+<?php 
+	get_header();
+
+ ?>
+
+<?php $image = get_field('featured_header'); ?>
+<div class="spacer"></div>
+<header class="standard--hdr">
+	<div class="inner">
+		<div class="hdr--group--default ">
+			<div class="hdr--liner-default group--hdr">
+				<h1><?php the_title(); ?></h1>
+				<p>By Jesse Adcock</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="is-hero--bg is-bg" data-bg-src="<?php echo $image['url'];?>"></div>
+</header>
+<main>
+
+
+	<section class="is-parent-el--padding no-padding-btm">
+	<div class="inner content--area">
+		<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+
+		<?php the_content(); ?>
+	<?php endwhile; endif; wp_reset_postdata(); ?>
+
+	<div class="back-btn">
+		<a href="<?php echo site_url('blog'); ?>" class="cta">View All Blogs</a>
+	</div>
+	</div>
+
+	
+</section>
+
+
+ <?php get_footer(); ?>
--- templates/blog.php
+++ templates/blog.php
@@ -1,17 +1,37 @@
 <?php /* Template Name: Blog */ ?>
 <?php get_header(); ?>
+<div class="spacer"></div>
+<header class="standard--hdr">
+	<div class="inner">
+		<div class="hdr--group--default ">
+			<div class="hdr--liner-default group--hdr">
+				<?php the_field('header_content'); ?>
+			</div>
+		</div>
+	</div>
 
+	<div class="is-hero--bg is-bg" data-bg-src="<?php the_field('header_background'); ?>"></div>
+</header>
+<main>
 <section class="is-parent-el--padding no-padding-btm">
 	<div class="inner content-area">
+
 		<?php 
 		$getPost = new WP_Query(array( 
 			'post_type' => 'post'
 
 		));
 		if($getPost -> have_posts()) : while($getPost -> have_posts()) : $getPost -> the_post();
+			$image = get_field('featured_header');
 		?>
 		<div class="blog-content">
-
+			 <a href="<?php the_permalink(); ?>"><figure class="galleryImg">
+				<img class="lazy-image is-gallery-image" src="" data-lazy="<?php echo $image['url'] ;?>" data-width="<?php echo $image['width'];  ?>" data-height="<?php echo $image['height']; ?>">
+			</figure></a>
+	<!-- 		<figure>
+				<?php echo $image['width']; ?>
+				<img src="<?php echo $image['url'];?>" alt="">
+			</figure> -->
 			<h1 class="blog-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 			<small>posted on: <?php the_date('F y Y') ?></small>
 			<div class="summary"><?php the_excerpt(''); ?></div>
